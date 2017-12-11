a7e6582b445e878bf28af4e82f58b420656f0355
Stephan van de Kerkhof <33581978+stephanvandekerkhof@users.noreply.github.com>
2017-12-11 22:53:05 +0100

    improved

--- p-x-t.bat
+++ p-x-t.bat
@@ -5,11 +5,12 @@ TITLE Password x-tract0r v0.04
 setlocal enabledelayedexpansion
 echo Password x-tract0r v0.04&&echo.
 set STARTDATETIME=%DATE% %TIME%
-set SPEECHMODE=0
+set SPEECHMODE=1
 
-for /r "%CD%" %%a IN (*) do (
+
+for /r "%CD%" %%a IN (test-data\*) do (
 echo Scanning in %%~nxa ^(%%a^) ...
-if [%SPEECHMODE%]==[1] dspeech.exeHidden /Speak Scanning in %%~nxa
+if [!SPEECHMODE!]==[1] dspeech.exe /Hidden /Speak Scanning in %%~nxa
 
 for /f "tokens=2 delims=:" %%i in (%%a) do (
 @echo off
@@ -17,30 +18,32 @@ echo Password x-tract0r v0.04 found: %%~nxa pwd: %%i
 echo %%i>>pwd.txt
 )
 
+find /v /c "" < "pwd_uniq_sorted.txt" > tmp
+set /p nr4=<tmp
+if not exist pwd_uniq_sorted.txt set nr4=0
+
 echo Done with "%%a"
+
+find /v /c "" < "pwd.txt" > tmp
+set /p nr1=<tmp
 
 sort pwd.txt | uniq >> pwd_uniq_sorted.txt
 
 find /v /c "" < "pwd_uniq_sorted.txt" > tmp
-set /p nr1=<tmp
-
-find /v /c "" < "pwd.txt" > tmp
-set /p nr4=<tmp
-
-echo !nr4! passwords found. comparing with previous results ...
-if [%SPEECHMODE%]==[1] dspeech.exeHidden /Speak !nr4! passwords found. comparing with previous results
+set /p nr2=<tmp
 
 sort pwd_uniq_sorted.txt | uniq > pwd_uniq_sorted_final.txt
 
 find /v /c "" < "pwd_uniq_sorted_final.txt" > tmp
-set /p nr2=<tmp
+set /p nr3=<tmp
 
-if exist tmp del tmp
-set /a nr3=!nr1!-!nr2!
+set /a nr5=!nr3!-!nr4!
 
-echo !nr3! new passwords found. Total harvest: !nr2!
-if [%SPEECHMODE%]==[1] dspeech.exeHidden /Speak !nr3! new passwords found. Total harvest !nr2!
-if [%SPEECHMODE%]==[0] timeout 3
+echo !nr1! passwords found inside %%~nxa.
+echo !nr5! new de-duplicated entries added.
+echo !nr2! total unique passwords found.
+if [!SPEECHMODE!]==[1] dspeech.exe /Hidden /Speak !nr1! passwords found in %%~nxa. Added !nr5! new de duplicated entries. Total unique passwords found !nr2!
+if [!SPEECHMODE!]==[0] timeout 3
 
 if exist pwd_uniq_sorted.txt del pwd_uniq_sorted.txt
 if exist pwd.txt del pwd.txt
@@ -57,7 +60,7 @@ echo Ended: %DATE% %TIME%
 echo Unique passwords extracted: %nr1%
 echo.
 echo Data extraction operation completed.
-start "" if [%SPEECHMODE%]==[1] dspeech.exeHidden /Speak Data extraction operation completed. Found %nr1% unique passwords
+if [%SPEECHMODE%]==[1] dspeech.exe /Hidden /Speak Data extraction operation completed. Found %nr1% unique passwords
 echo.
 pause
 if exist tmp del tmp
